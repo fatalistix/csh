44e80af80b8b4816744a2ecb4e2a6633a17b5e59
fatalistix <slavabal11@outlook.com>
2024-07-26 20:34:29 +0700

    reformatted code

--- src/util/csh_check_and_reallock.c
+++ src/util/csh_check_and_reallock.c
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int csh_check_and_realloc(void** ptr, size_t* size, size_t pos, size_t cap_step, size_t size_of) {
-    if (pos >= *size) {
-        size_t new_size = *size + cap_step;
-        *ptr = realloc(*ptr, new_size * size_of);
-        if (!*ptr) {
-            return CSH_CHECK_AND_REALLOCK_ALLOCATION_ERR;
-        }
+int csh_check_and_realloc(void **ptr, size_t *size, size_t pos, size_t cap_step,
+						  size_t size_of) {
+	if (pos >= *size) {
+		size_t new_size = *size + cap_step;
+		*ptr = realloc(*ptr, new_size * size_of);
+		if (!*ptr) {
+			return CSH_CHECK_AND_REALLOCK_ALLOCATION_ERR;
+		}
 
-        *size = new_size;
-    }
+		*size = new_size;
+	}
 
-    return CSH_CHECK_AND_REALLOCK_OK;
+	return CSH_CHECK_AND_REALLOCK_OK;
 }
