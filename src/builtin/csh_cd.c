428a1b739ff0e916e22fceee5fea23149890760a
fatalistix <slavabal11@outlook.com>
2024-07-26 20:30:17 +0700

    func sets E2BIG to ERRNO and returns CHECK_ERRNO

--- src/builtin/csh_cd.c
+++ src/builtin/csh_cd.c
@@ -1,18 +1,20 @@
 #include "csh_builtin.h"
 
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int csh_cd(char** args) {
-    if (args[1] == NULL) {
-        return CSH_BUILTIN_OK;
-    }
-    if (args[2] != NULL) {
-        return CSH_BUILTIN_TOO_MANY_ARGS;
-    }
-    if (chdir(args[1]) != 0) {
-        return CSH_BUILTIN_CHECK_ERRNO;
-    }
+int csh_cd(char **args) {
+	if (args[1] == NULL) {
+		return CSH_BUILTIN_OK;
+	}
+	if (args[2] != NULL) {
+		errno = E2BIG;
+		return CSH_BUILTIN_CHECK_ERRNO;
+	}
+	if (chdir(args[1]) != 0) {
+		return CSH_BUILTIN_CHECK_ERRNO;
+	}
 
-    return CSH_BUILTIN_OK;
+	return CSH_BUILTIN_OK;
 }
