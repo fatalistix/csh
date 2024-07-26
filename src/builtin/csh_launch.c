8735c9b3010f423af49e19c2138db4f7c9c40ed1
fatalistix <slavabal11@outlook.com>
2024-07-26 20:32:17 +0700

    reformatted code

--- src/builtin/csh_launch.c
+++ src/builtin/csh_launch.c
@@ -6,18 +6,18 @@
 #include <unistd.h>
 
 int csh_launch(char **args) {
-    pid_t pid;
+	pid_t pid;
 	pid_t wpid;
 	int status;
 
 	pid = fork();
 	if (pid == 0) {
 		if (execvp(args[0], args) == -1) {
-		    exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
 		}
 		exit(EXIT_SUCCESS);
 	} else if (pid < 0) {
-	    return CSH_BUILTIN_CHECK_ERRNO;
+		return CSH_BUILTIN_CHECK_ERRNO;
 	} else {
 		do {
 			wpid = waitpid(pid, &status, WUNTRACED);
